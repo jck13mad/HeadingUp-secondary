a933e574153574d4e95fece32336b2b178e69d32
John Madden <johnmadden@Johns-MacBook-Pro.local>
2020-12-07 10:21:22 -0600

    Added login and signup erb

--- Desktop/HeadingUp/app/controllers/application_controller.rb
+++ Desktop/HeadingUp/app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 
 class ApplicationController < Sinatra::Base
 
-  set :views, -> { File.join(root, '../views')}
+  # set :views, -> { File.join(root, '../views')}
+
+  configure do
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'secret'
+  end
 
   
+
+
 
   get '/' do
      erb :'home'
