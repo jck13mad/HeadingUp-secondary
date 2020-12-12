607a7b7a7e72dcaded07c18f718b99dd8d1326ca
John Madden <jck13mad@gmail.com>
2020-12-12 12:01:24 -0600

    Flash error messages for failed sign ins

--- Desktop/HeadingUp/app/controllers/user_controller.rb
+++ Desktop/HeadingUp/app/controllers/user_controller.rb
@@ -9,5 +9,18 @@ class UserController < ApplicationController
     user = User.where(email: params[:user][:email])
 
     if !user.empty?
+      flash[:message] = "Email not available, please use a different email."
+      redirect '/signup'
+    elsif empty_fields?(params[:user])
+      flash[:message] = "All fields must be filled."
+      redirect '/signup'
+    else
+      u = User.new(params[:user])
+
+      if u.save 
+        session[:user_id] = u.id
+        redirect '/all_heads'
+      else
+        flash[:message] = "Uh-oh, that didn't go to plan. "
   end
 end
